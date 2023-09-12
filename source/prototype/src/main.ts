3793c543dec82e806291e0b7478a83440c3acf33
Krystof Hruby <hrubyk97@gmail.com>
2023-09-12 13:04:34 +0200

    update schema and create crude implementation.

--- source/prototype/src/main.ts
+++ source/prototype/src/main.ts
@@ -1,17 +1,133 @@
 import fs from 'fs/promises';
-import { parseSchema } from './schema-parser';
-
+// import { parseSchema } from './schema-parser';
+import { SchemaModel, IEntity, Entity, IProperty, Property, ILiteral, Literal } from './schema';
+import { Command, MovePropertyCommand } from './command';
+import {
+    IInstanceModel,
+    InMemoryInstanceModel,
+    IEntityInstance,
+    EntityInstance,
+    IPropertyInstance,
+    PropertyInstance,
+    ILiteralInstance,
+    LiteralInstance,
+} from './instance';
 const args = process.argv.slice(2);
+// require('util').inspect.defaultOptions.depth = null;
 
-async function main() {
-    console.log('args:', args);
-    require('util').inspect.defaultOptions.depth = null;
-    const data = await fs.readFile(args[0]);
-    const obj = JSON.parse(data.toString())[0];
-    const [schema, entities, literals, instances, schemaInstanceMap]: any = parseSchema(obj);
-    // console.log(JSON.stringify(schema));
-    // console.log('entities:', entities);
-    console.log(schema);
+// async function main() {
+//     console.log('args:', args);
+//     const data = await fs.readFile(args[0]);
+//     const obj = JSON.parse(data.toString())[0];
+//     const [schema, entities, literals, instances, schemaInstanceMap]: any = parseSchema(obj);
+//     // console.log(JSON.stringify(schema));
+//     // console.log('entities:', entities);
+//     console.log(schema);
+// }
+
+// main();
+schemaAndInstanceCommandExample();
+
+function schemaAndInstanceCommandExample() {
+    /**
+     * {
+     *      name: 'noodles',
+     *      calcium_100g: '20',
+     *      nutrients: {
+     *          magnesium_100g: '30'
+     *      }
+     * }
+     * TO
+     * {
+     *      name: 'noodles',
+     *      nutrients: {
+     *          calcium_100g: '20',
+     *          magnesium_100g: '30'
+     *      }
+     * }
+     */
+
+    const schema = new SchemaModel(null);
+    const nameLiteral = new Literal('10');
+    const nameProperty = new Property(schema, '100', 'name', nameLiteral.getId());
+    const calciumLiteral = new Literal('11');
+    const calciumProperty = new Property(schema, '101', 'calcium_100g', calciumLiteral.getId());
+    const magnesiumLiteral = new Literal('12');
+    const magnesiumProperty = new Property(schema, '102', 'magnesium_100g', magnesiumLiteral.getId());
+
+    const nutrientsEntity = new Entity(schema, '1', [magnesiumProperty.getId()]);
+    const nutrientsProperty = new Property(schema, '103', 'nutrients', nutrientsEntity.getId());
+    const productEntity = new Entity(schema, '2', [nameProperty.getId(), calciumProperty.getId(), nutrientsProperty.getId()]);
+
+    schema.addLiteral(nameLiteral);
+    schema.addLiteral(calciumLiteral);
+    schema.addLiteral(magnesiumLiteral);
+
+    schema.addProperty(nameProperty);
+    schema.addProperty(calciumProperty);
+    schema.addProperty(magnesiumProperty);
+    schema.addProperty(nutrientsProperty);
+
+    schema.addEntity(nutrientsEntity);
+    schema.addEntity(productEntity);
+
+    const instanceSchema = new InMemoryInstanceModel(null);
+    const noodlesInstanceLiteral = new LiteralInstance('10', 'noodles');
+    const noodlesInstanceProperty = new PropertyInstance(instanceSchema, '100', noodlesInstanceLiteral.getId());
+    const calciumInstanceLiteral = new LiteralInstance('11', '20');
+    const calciumInstanceProperty = new PropertyInstance(instanceSchema, '101', calciumInstanceLiteral.getId());
+    const magnesiumInstanceLiteral = new LiteralInstance('12', '30');
+    const magnesiumInstanceProperty = new PropertyInstance(instanceSchema, '102', magnesiumInstanceLiteral.getId());
+
+    const nutrientsInstanceEntity = new EntityInstance(instanceSchema, '1', [magnesiumInstanceProperty.getId()]);
+    const nutrientsInstanceProperty = new PropertyInstance(instanceSchema, '103', nutrientsInstanceEntity.getId());
+    const productInstanceEntity = new EntityInstance(instanceSchema, '2', [
+        noodlesInstanceProperty.getId(),
+        calciumInstanceProperty.getId(),
+        nutrientsInstanceProperty.getId(),
+    ]);
+
+    instanceSchema.addLiteral(noodlesInstanceLiteral);
+    instanceSchema.addLiteral(calciumInstanceLiteral);
+    instanceSchema.addLiteral(magnesiumInstanceLiteral);
+
+    instanceSchema.addProperty(nameProperty, noodlesInstanceProperty);
+    instanceSchema.addProperty(calciumProperty, calciumInstanceProperty);
+    instanceSchema.addProperty(magnesiumProperty, magnesiumInstanceProperty);
+    instanceSchema.addProperty(nutrientsProperty, nutrientsInstanceProperty);
+
+    instanceSchema.addEntity(nutrientsEntity, nutrientsInstanceEntity);
+    instanceSchema.addEntity(productEntity, productInstanceEntity);
+
+    const moveCommand = new MovePropertyCommand(schema, instanceSchema, calciumProperty, productEntity, nutrientsEntity);
+    const [newSchema, newInstanceModel] = moveCommand.execute();
+    console.log(newSchema.entity(nutrientsEntity.getId()));
+    console.log(newSchema.entity(nutrientsEntity.getId())?.getProperties());
+    console.log(newInstanceModel.entity(nutrientsInstanceEntity.getId()));
+    console.log(newInstanceModel.entity(nutrientsInstanceEntity.getId()).getProperties());
 }
 
-main();
+function schemaModelCommandExample() {
+    /**
+     * { id: 1, label => { id: 2, calcium => { id: 4 } }, knows => { id: 3, calcium => { id: 4 } } }
+     * to
+     * { id: 1, label => { id: 2, calcium => { id: 4 }, knows => { id: 3, calcium => { id: 4 } } } }
+     */
+    const schema = new SchemaModel(null);
+    schema.addEntity(new Entity(schema, '1', ['1', '2']));
+    schema.addEntity(new Entity(schema, '2', ['3']));
+    schema.addEntity(new Entity(schema, '3', ['3']));
+    schema.addLiteral(new Literal('4'));
+    schema.addProperty(new Property(schema, '1', 'label', '2'));
+    schema.addProperty(new Property(schema, '2', 'knows', '3'));
+    schema.addProperty(new Property(schema, '3', 'calcium', '4'));
+
+    const moveCommand = new MovePropertyCommand(schema, new InMemoryInstanceModel(), schema.property('2')!, schema.entity('1')!, schema.entity('2')!);
+    const newSchema = moveCommand.execute();
+    // console.log(newSchema.entity('1'));
+    // console.log(schema.entity('1'));
+    // console.log(newSchema.entity('2'));
+    // console.log(schema.entity('2'));
+    // console.log(newSchema.entity('3'));
+    // console.log(schema.entity('3'));
+}
--- source/prototype/src/schema.ts
+++ source/prototype/src/schema.ts
@@ -1,45 +1,182 @@
-export interface EntityInstance {
-    id: string;
-    // uri: string;
+import { IInstanceModel, InMemoryInstanceModel } from './instance';
+
+export interface IEntity {
+    getId(): string;
+    addProperty(schemaModel: ISchemaModel, property: IProperty): IEntity;
+    removeProperty(schemaModel: ISchemaModel, property: IProperty): IEntity;
+    getProperties(): IProperty[];
 }
 
-export interface PropertyInstance {
-    id: string;
-    // uri: string;
-}
-export interface LiteralInstance {
-    id: string;
-    // uri: string;
+export class Entity implements IEntity {
+    constructor(
+        private schemaModel: ISchemaModel,
+        private id: string,
+        private propertyIds: string[]
+    ) {}
+
+    getId(): string {
+        return this.id;
+    }
+    addProperty(schemaModel: ISchemaModel, property: IProperty): IEntity {
+        const clone = this.clone(schemaModel);
+        clone.propertyIds.push(property.getId());
+        return clone;
+    }
+    removeProperty(schemaModel: ISchemaModel, property: IProperty): IEntity {
+        const clone = this.clone(schemaModel);
+        clone.propertyIds = clone.propertyIds.filter((propertyId) => propertyId !== property.getId());
+        return clone;
+    }
+    getProperties(): IProperty[] {
+        return this.propertyIds.map((propertyId) => this.schemaModel.property(propertyId)).filter((property) => property != null) as IProperty[];
+    }
+
+    private clone(schemaModel: ISchemaModel): Entity {
+        return new Entity(schemaModel, this.id, [...this.propertyIds]);
+    }
 }
 
-export interface Instance {
-    id: string;
-    value: string;
+export interface IProperty {
+    getId(): string;
+    getUri(): string | undefined;
+    getName(): string;
+    getValue(): IEntity | ILiteral;
+    changeValue(schemaModel: ISchemaModel, value: IEntity | ILiteral): IProperty;
 }
 
-export interface Entity {
-    id: string;
-    properties: Property[];
+export class Property implements IProperty {
+    constructor(
+        private schemaModel: ISchemaModel,
+        private id: string,
+        private name: string,
+        private valudId: string,
+        private uri?: string
+    ) {}
+    getId(): string {
+        return this.id;
+    }
+    getUri(): string | undefined {
+        return this.uri;
+    }
+    getName(): string {
+        return this.name;
+    }
+    getValue(): IEntity | ILiteral {
+        throw new Error('Method not implemented.');
+    }
+    changeValue(schemaModel: ISchemaModel, value: IEntity | ILiteral): IProperty {
+        const clone = this.clone(schemaModel);
+        clone.valudId = value.getId();
+        throw new Error('Method not implemented.');
+    }
 
-    // getInstances(): EntityInstance[];
+    private clone(schemaModel: ISchemaModel): Property {
+        return new Property(schemaModel, this.id, this.name, this.valudId);
+    }
 }
-export interface Property {
-    name: string;
-    // uri: string;
-    value: Entity | Literal;
 
-    // getInstances(entity: EntityInstance): EntityInstance[] | LiteralInstance[];
+export interface ILiteral {
+    getId(): string;
 }
-export interface Literal {
-    id: string;
-    // getInstances(): LiteralInstance[];
+
+export class Literal implements ILiteral {
+    constructor(private id: string) {}
+    getId(): string {
+        return this.id;
+    }
 }
 
 export interface InstanceStore {
     // Store for data in the form of EntityInstance, PropertyInstance, LiteralInstance.
 }
 
-export interface BaseModel {
-    entities: Entity[];
-    properties: Property[];
+export interface ISchemaModel {
+    entities(): IEntity[];
+    properties(): IProperty[];
+    literals(): ILiteral[];
+    entity(id: string): IEntity | null;
+    property(id: string): IProperty | null;
+    literal(id: string): ILiteral | null;
+
+    getBaseSchemaModel(): ISchemaModel | null;
+}
+
+// export interface InstanceModel {
+//     entities(): EntityInstance[];
+//     properties(): PropertyInstance[];
+//     literals(): LiteralInstance[];
+
+// }
+
+export class SchemaModel implements ISchemaModel {
+    private _entities = new Map<string, IEntity>();
+    private _properties = new Map<string, IProperty>();
+    private _literals = new Map<string, ILiteral>();
+    private _baseSchemaModel: ISchemaModel | null;
+    constructor(baseSchemaModel: ISchemaModel | null = null) {
+        this._baseSchemaModel = baseSchemaModel;
+    }
+
+    addEntity(entity: IEntity): void {
+        this._entities.set(entity.getId(), entity);
+    }
+
+    addProperty(property: IProperty): void {
+        this._properties.set(property.getId(), property);
+    }
+
+    addLiteral(literal: ILiteral): void {
+        this._literals.set(literal.getId(), literal);
+    }
+
+    entities(): IEntity[] {
+        throw new Error('Method not implemented.');
+    }
+    properties(): IProperty[] {
+        throw new Error('Method not implemented.');
+    }
+    literals(): ILiteral[] {
+        throw new Error('Method not implemented.');
+    }
+    // TODO: handle deleted entities
+    entity(id: string): IEntity | null {
+        const entity = this._entities.get(id);
+        if (entity) {
+            return entity;
+        }
+
+        if (this._baseSchemaModel) {
+            return this._baseSchemaModel.entity(id);
+        }
+
+        return null;
+    }
+    // TODO: handle deleted properties
+    property(id: string): IProperty | null {
+        const property = this._properties.get(id);
+        if (property) {
+            return property;
+        }
+
+        if (this._baseSchemaModel) {
+            return this._baseSchemaModel.property(id);
+        }
+
+        return null;
+    }
+    // TODO: handle deleted literals
+    literal(id: string): ILiteral | null {
+        const literal = this._literals.get(id);
+        if (literal) {
+            return literal;
+        }
+
+        if (this._baseSchemaModel) {
+            return this._baseSchemaModel.literal(id);
+        }
+        return null;
+    }
+    getBaseSchemaModel(): ISchemaModel | null {
+        return this._baseSchemaModel;
+    }
 }
