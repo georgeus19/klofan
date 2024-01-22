f69782c0202365bce6c58421c636ee760d56617e
Krystof Hruby <hrubyk97@gmail.com>
2024-01-22 14:32:59 +0100

    Remove old prototype source codes.

--- source/oop-prototype/src/utils.ts
+++
@@ -1,7 +0,0 @@
-export function isPrimitiveType(x: unknown): boolean {
-    return x !== Object(x);
-}
-
-export function isNotPrimitiveType(x: unknown): boolean {
-    return !isPrimitiveType(x);
-}
--- source/prototype/src/export/rdf-terms.ts
+++
@@ -1,1 +0,0 @@
-export const rdfType = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type';
